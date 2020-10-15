5c5810d837068e6c0025ce8e61be526f536883b4
Opilane TTHK <opilane@tthk.ee>
2020-10-15 11:34:21 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IDCodeReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
