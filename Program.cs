62fce46e6e2dece5728817b5960ea758a4ad19b3
darkxyz-dev <70570865+darkxyz-dev@users.noreply.github.com>
2020-10-21 15:49:33 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,54 @@ namespace IDCodeReader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your ID code:");
+            string usersID = Console.ReadLine();
+
+            if (Validate(usersID))
+            {
+                Console.WriteLine("Welcome!");
+                HelloUser(usersID);
+            }
+            else
+            {
+                Console.WriteLine("Wrong format. Try again.");
+            }
+        }
+        public static bool Validate(string idCode)
+        {
+            if (idCode.Length == 11)
+            {
+                try
+                {
+                    long.Parse(idCode);
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Wrong formar: {e}");
+                    return false;
+                }
+
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public static void HelloUser(string idCode)
+        {
+            int firstNum = Int32.Parse(idCode[0].ToString());
+            
+            if (firstNum == 1 || firstNum == 3 || firstNum == 5)
+            {
+                Console.WriteLine("Hello, Sir!");
+            }
+            else if (firstNum == 2 || firstNum == 4 || firstNum == 6)
+            {
+                Console.WriteLine("Hello, Madam!");
+            }
         }
     }
 }
